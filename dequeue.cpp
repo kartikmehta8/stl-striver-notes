1dcc98c9a1faf5f462fccf4e814cfa4b61dca574
Kartik Mehta <kartikmehta202@gmail.com>
2022-01-28 06:53:41 +0530

    DEQUEUE

---
+++ dequeue.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+//Ctrl+Alt+F    To format the code
+//Ctrl+B        To run the code
+//Ctrl+/        To comment the lines completly
+//Ctrl+Shift+/  To comment the required/selected lines only
+
+using namespace std;
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	//DEQUEUE STL
+
+	//DECLARING
+	dequeue<int> dq;
+
+	//OPERATIONS
+	// 1. push_front()
+	// 2. push_back()
+	// 3. pop_front()
+	// 4. pop_back()
+	// 5. begin()
+	// 6. end()
+	// 7. rbegin()
+	// 8. rend()
+	// 9. size()
+	// 10. cear()
+	// 11. empty()
+	// 12. at()
+
+	//LIST STL
+
+	//DECLARING
+	list<int> ls;
+
+	//OPERATIONS
+	// 1. push_front()
+	// 2. push_back()
+	// 3. pop_front()
+	// 4. pop_back()
+	// 5. begin()
+	// 6. end()
+	// 7. rbegin()
+	// 8. rend()
+	// 9. size()
+	// 10. cear()
+	// 11. empty()
+	// 12. at()
+	// 13. remove() TIME COMPLEXITY - O(1) (FAST)
+
+	return 0;
+}
